4f0b0328362f287fcf4a46b0739558d1c9b7ca41
talmiller2 <talmiller@gmail.com>
2021-01-11 17:03:18 +0200

    plot script was not added in last commit

---
+++ plot_simulation_rebalance_synthetic.py
@@ -0,0 +1,119 @@
+import matplotlib
+from data_functions import load_stock_data, define_stock_parameters
+from aux_functions import get_year_labels
+from settings_functions import define_default_settings
+from market_functions import simulate_portfolio_evolution
+from cycler import cycler
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# matplotlib.use('TkAgg')
+
+plt.rcParams.update({'font.size': 12})
+matplotlib.rcParams.update({'axes.prop_cycle': cycler(color='bgrcmyk')})
+# matplotlib.rcParams.update({'axes.prop_cycle': cycler(color='bcmygrk')})
+
+# plt.close('all')
+
+# date_start = '1929-01-01'
+# date_start = '1986-01-01'
+# date_start = '1987-01-01'
+date_start = '1989-01-01'
+# date_start = '1993-01-29'
+# date_start = '1999-12-30'
+# date_start = '2000-01-01'
+# date_start = '2001-01-01'
+# date_start = '2002-01-01'
+# date_start = '2003-01-01'
+# date_start = '2010-01-01'
+# date_start = '2011-01-01'
+
+# date_end = '2001-01-01'
+date_end = '2020-09-30'
+# date_end = '1996-09-30'
+
+### plot data section
+
+stock_name_list = []
+# stock_name_list += ['SP500']
+# stock_name_list += ['SP500TR']
+# stock_name_list += ['SPY']
+# stock_name_list += ['VOO']
+# stock_name_list += ['IVV']
+# stock_name_list += ['UPRO']
+
+# stock_name_list += ['NDX100']
+stock_name_list += ['NDX100TR']
+# stock_name_list += ['QQQ']
+# stock_name_list += ['TQQQ']
+
+# stock_name_list += ['TLT']
+stock_name_list += ['TLT-TR']
+# stock_name_list += ['VUSTX']
+# stock_name_list += ['VBTLX']
+# stock_name_list += ['TMF']
+
+### simulation section
+
+# plot_sim = False
+plot_sim = True
+
+if plot_sim:
+    settings = define_default_settings()
+    settings['date_start'] = date_start
+    settings['date_end'] = date_end
+    # settings['ideal_portfolio_fractions'] = {'SP500': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'VOO': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'UPRO': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'QQQ': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'TQQQ': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'TLT': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'TMF': 1.0}
+    # settings['ideal_portfolio_fractions'] = {'VOO': 0.5, 'QQQ': 0.5}
+    # settings['ideal_portfolio_fractions'] = {'VOO': 0.5, 'TLT': 0.5}
+    # settings['ideal_portfolio_fractions'] = {'UPRO': 0.5, 'TQQQ': 0.5}
+    settings['ideal_portfolio_fractions'] = {'QQQ': 0.5, 'TQQQ': 0.5}
+    # settings['ideal_portfolio_fractions'] = {'UPRO': 0.5, 'TMF': 0.5}
+    # settings['ideal_portfolio_fractions'] = {'TQQQ': 0.5, 'TMF': 0.5}
+    # settings['periodic_investment_interval'] = 'yearly'
+    # settings['periodic_investment_interval'] = 'quarterly'
+    # settings['capital_gains_tax_percents'] = 0
+    # settings['capital_gains_tax_percents'] = 5
+    # settings['transaction_fee_percents'] = 0
+    # settings['tax_scheme'] = 'FIFO'
+    # settings['tax_scheme'] = 'LIFO'
+    settings['tax_scheme'] = 'optimized'
+    settings['perform_bootstrap'] = True
+    data = simulate_portfolio_evolution(settings)
+
+    # plots
+    plt.figure(1)
+    # label = 'sim tax ' + settings['tax_scheme']
+    label = 'sim tax ' + settings['tax_scheme'] + ' (total sell tax loss ' + '{:0.2f}'.format(data['total_sell_tax_loss_percents']) + '%)'
+    # plt.plot(data['total_portfolio_value'], label=label, linewidth=2)
+    # plt.plot(data['total_portfolio_value'], label=label, linewidth=2, color='k', zorder=1)
+    plt.plot(data['total_portfolio_value'] / data['total_investment'], label=label, linewidth=2, color='k', zorder=1)
+    # plt.scatter(data['papers_buy_days'], data['portfolio_values_at_buy_days'], s=2, color=data['papers_status_colors'], zorder=2)
+    # plt.plot(data['total_portfolio_value'] + data['cash_in_account'], label=label + ' + divs not reinvested', linewidth=2)
+    # plt.plot(data['cash_in_account'], label='cash_in_account', linewidth=2)
+    # plt.yscale('log')
+    plt.xticks(data['inds_years'], data['label_years'])
+    plt.xlabel('years')
+    plt.ylabel('yield')
+    # plt.title('Stock Data')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.figure(2)
+    for stock_name in data['portfolio_fractions'].keys():
+        plt.plot(data['portfolio_fractions'][stock_name], label=stock_name, linewidth=2)
+    plt.xticks(data['inds_years'], data['label_years'])
+    plt.title('portfolio fractions')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+
+
