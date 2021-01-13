ea4bbfaf636a6e51a05d1f1279397c33f4d0a2c5
talmiller2 <talmiller@gmail.com>
2021-01-13 13:41:41 +0200

    fixes

--- bootstrap_portfolio_slave.py
+++ bootstrap_portfolio_slave.py
@@ -5,7 +5,7 @@ import ast
 import os
 import numpy as np
 
-from  market_functions import simulate_portfolio_evolution
+from market_functions import simulate_portfolio_evolution
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--settings', help='settings (dict) for the portfolio simulation algorithm',
@@ -15,18 +15,25 @@ parser.add_argument('--bootstrap_params', help='parameters (dict) for the bootst
 
 args = parser.parse_args()
 print('args.settings = ' + str(args.settings))
-print('args.settings = ' + str(args.bootstrap_params))
+print('args.bootstrap_params = ' + str(args.bootstrap_params))
 settings = ast.literal_eval(args.settings)
 bootstrap_params = ast.literal_eval(args.bootstrap_params)
 
+# define log file
+os.makedirs(bootstrap_params['save_dir'], exist_ok=True)
+log_file_path = bootstrap_params['save_dir'] + '/log_' + settings['sim_name'] + '.txt'
+log_file = open('filename.txt', 'w')
+
+# perform bootstrap loop
 yield_list = []
 for ind_real in range(bootstrap_params['num_realizations']):
     data = simulate_portfolio_evolution(settings)
     label = 'real ' + str(ind_real) + ', yield=' + '{:0.2f}'.format(data['total_yield'])
-    print(label)
+    print(label, file=log_file)
     yield_list += [data['total_yield']]
 
 # save results to file
-os.makedirs(bootstrap_params['save_dir'], exist_ok=True)
-file_name = bootstrap_params['save_dir'] + '/' + bootstrap_params['sim_name'] + '.txt'
-np.savetxt(file_name, yield_list)
+save_file_path = bootstrap_params['save_dir'] + '/' + bootstrap_params['sim_name'] + '.txt'
+np.savetxt(save_file_path, yield_list)
+
+log_file.close()
